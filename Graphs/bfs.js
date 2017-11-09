3e7176cccc7973441d7760a89d6e326e8045a453
Jeff Shindelus <jeff.shindelus@gmail.com>
2017-11-08 21:21:40 -0800

    Implement BFSearch

--- Graphs/bfs.js
+++ Graphs/bfs.js
@@ -1,1 +1,59 @@
 // BREADTH FIRST SEARCH
+
+
+let BFSearch = (tree, value) => {
+  let queue = [tree];
+  while (queue.length > 0) {
+    let tempNode = queue.shift();
+    if (tempNode.value === value) {
+      return tempNode;
+    }
+    if (tempNode.left) {
+      queue.push(tempNode.left);
+    }
+    if (tempNode.right) {
+      queue.push(tempNode.right);
+    }
+  }
+  return null;
+}
+
+let t = {
+    "value": 4,
+    "left": {
+        "value": 1,
+        "left": {
+            "value": -2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": null
+    },
+    "right": {
+        "value": 3,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": {
+                "value": -2,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": -3,
+                "left": null,
+                "right": null
+            }
+        }
+    }
+}
+
+console.log(BFSearch(t, -3));
